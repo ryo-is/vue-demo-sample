e287ad67047bd6e5951305a3ec44db0163d84778
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-01-06 19:44:00 +0900

    feat:下準備

---
+++ src/app/App.vue
@@ -0,0 +1,3 @@
+<template src="./app.pug" lang="pug"></template>
+
+<style src="./app.scss" lang="scss"></style>
---
+++ src/views/home/home.ts
@@ -0,0 +1,6 @@
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Home extends Vue {
+  public homeTitle: string = "Vue Demo Sapmle";
+}
