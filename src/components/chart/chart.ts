b39b628f0651a17aa13ac88fde2e78b0849ae6a6
ryosuke izumi <ryosuke_izumi@kyoso.co.jp>
2019-01-15 13:03:20 +0900

    feat:vue-chartjsのデモ

--- src/components/chart/chart.ts
+++ src/components/chart/chart.ts
@@ -1,26 +1,90 @@
-import { Vue, Component, Mixins } from "vue-property-decorator";
-import { Bar, Line } from "vue-chartjs";
+import { Component, Mixins, Prop, Watch } from "vue-property-decorator";
+import { Line, mixins } from "vue-chartjs";
+// const { reactiveProp } = mixins;
 
 @Component({})
-export default class ChartComponent extends Mixins(Bar) {
-  public chartData: any = {
-    datacollection: {
-      labels: ["January", "February"],
-      datasets: [
-        {
-          label: "Data One",
-          backgroundColor: "#f87979",
-          data: [40, 20]
-        }
-      ]
+export default class ChartComponent extends Mixins(Line, mixins.reactiveProp) {
+  @Prop() public chartData: any;
+
+  @Watch("chartData") public onChangeChartValue() {
+    console.log(this);
+  }
+
+  // public datacollection: any = {
+  //   labels: ["January", "February"],
+  //   datasets: [
+  //     {
+  //       label: "Data One",
+  //       backgroundColor: "#f87979",
+  //       data: this.chartData
+  //     }
+  //   ]
+  // };
+
+  public options: any = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      // display: false
+      onClick(event, legendItem) {
+        return;
+      },
+      fullWidth: false,
+      labels: {
+        boxWidth: 15,
+        fontColor: "#ffffff"
+      },
     },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false
+    layout: {
+      padding: {
+        top: 20,
+        left: 20,
+        bottom: 20,
+        right: 20
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            autoSkip: true,
+            fontColor: "rgba(255, 255, 255, 1)",
+            fontSize: 14
+          },
+          scaleLabel: {
+            display: true,
+            fontColor: "rgba(255, 255, 255, 1)",
+            labelString: "月",
+          },
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            display: true,
+            color: "rgba(255, 255, 255, .5)",
+            zeroLineColor: "rgba(255, 255, 255, 1)"
+          },
+          scaleLabel: {
+            display: false,
+          },
+          ticks: {
+            autoSkip: true,
+            fontColor: "rgba(255, 255, 255, 1)",
+            fontSize: 14,
+            max: 100,
+            min: 0
+          },
+        },
+      ],
     }
   };
 
   public mounted() {
-    this.renderChart(this.chartData.datacollection, this.chartData.options);
+    console.log(this);
+    this.renderChart(this.chartData, this.options);
   }
 }
--- src/views/home/home.ts
+++ src/views/home/home.ts
@@ -42,11 +42,11 @@ export default class Home extends Vue {
       value: 20
     }
   ];
+  public chartData: any = {};
 
-  // public created() {
-  //   const refDemoComponent: refComponent = this.$refs.refDemo as Vue;
-  //   console.log(refDemoComponent); // undefined
-  // }
+  public created() {
+    this.createChartValue();
+  }
 
   public mounted() {
     const refDemoComponent: any = this.$refs.refDemo;
@@ -60,5 +60,35 @@ export default class Home extends Vue {
   public changeRefInputText() {
     const refDemoComponent: any = this.$refs.refDemo;
     refDemoComponent.updateRefText(this.refInputText);
+  }
+
+  public createChartValue() {
+    this.chartData = {
+      labels: ["1", "2", "3", "4", "5"],
+      datasets: [
+        {
+          label: "Tokyo",
+          backgroundColor: "#F87979",
+          borderColor: "#F87979",
+          fill: false,
+          data: this.craeteRamdomValue()
+        },
+        {
+          label: "Osaka",
+          backgroundColor: "#6090EF",
+          borderColor: "#6090EF",
+          fill: false,
+          data: this.craeteRamdomValue()
+        }
+      ]
+    };
+  }
+
+  public craeteRamdomValue() {
+    const arr: number[] = [];
+    for (let i = 0; i < 5; i++) {
+      arr.push(Math.random() * 100);
+    }
+    return arr;
   }
 }
